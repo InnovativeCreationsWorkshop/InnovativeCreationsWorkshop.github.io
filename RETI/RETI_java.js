fad0cc47948ebae2c1fc829b5b21c2e94d3f8162
InnovativeCreationsWorkshop <159676158+InnovativeCreationsWorkshop@users.noreply.github.com>
2024-03-05 13:21:17 -0700

    Create RETI_java.js

---
+++ RETI/RETI_java.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const imageFolder = 'images/';
+
+const imageGrid = document.getElementById('imageGrid');
+
+fs.readdir(imageFolder, (err, files) => {
+    if (err) {
+        console.error('Error reading directory:', err);
+        return;
+    }
+
+    files.forEach(file => {
+        if (file.match(/\.(jpg|jpeg|png|gif)$/)) {
+            const imagePath = path.join(imageFolder, file);
+            const imageElement = document.createElement('img');
+            imageElement.src = imagePath;
+            imageElement.alt = 'Image';
+            
+            const imageItem = document.createElement('div');
+            imageItem.classList.add('image-item');
+            imageItem.appendChild(imageElement);
+
+            imageGrid.appendChild(imageItem);
+        }
+    });
+});
