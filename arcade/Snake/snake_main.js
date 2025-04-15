c96c70f855e8d752888ec028845409361a026f42
InnovativeCreationsWorkshop <159676158+InnovativeCreationsWorkshop@users.noreply.github.com>
2025-04-15 15:22:07 -0600

    Update snake_main.js

--- arcade/Snake/snake_main.js
+++ arcade/Snake/snake_main.js
@@ -1,5 +1,4 @@
-import { startGame, setupGame } from './Snake/gameSetup.js';
-import { theGame } from './Snake/gameLoop.js';
+
 
 const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
