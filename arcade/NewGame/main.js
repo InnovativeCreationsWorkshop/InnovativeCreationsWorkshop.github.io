da6a6496ec1b134dbd944fa8dec0fb61a7469de1
InnovativeCreationsWorkshop <159676158+InnovativeCreationsWorkshop@users.noreply.github.com>
2025-04-24 11:25:07 -0600

    Update main.js

--- arcade/NewGame/main.js
+++ arcade/NewGame/main.js
@@ -15,40 +15,7 @@ function draw() {
   updateAndDrawFlowers();
 }
 
-function mousePressed() {
-  let flower = createFlower();
 
-  // reassign x to be mouseX
-  flower.x = mouseX; 
-  
-  // reassign y to be mouseY
-  flower.y = mouseY;
-
-  // add the flower to the flowers array
-  flowers.push(flower);
-}
-
-function updateAndDrawFlowers() {
-  for (let flower of flowers) {
-
-    // Draw the flower.
-    drawFlower(flower);
-
-    // Apply wilting effect by reducing size by 1%
-    flower.size *= 0.99;
-
-    // Reduce lifespan
-    flower.lifespan -= 1;
-    
-    if (flower.lifespan <= 0) {
-      // Save index of the flower.
-      let i = flowers.indexOf(flower);
-      
-      // Remove wilted flower.
-      flowers.splice(i, 1);
-    }
-  }
-}
 
 // Function to create 20 flowers.
 function flowerPower(){
