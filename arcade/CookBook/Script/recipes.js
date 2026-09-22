a73a9d3daee269430f0ed0630e2d3360b8bdfe5b
InnovativeCreationsWorkshop <159676158+InnovativeCreationsWorkshop@users.noreply.github.com>
2026-09-22 16:15:54 -0600

    Update recipes.js

--- arcade/CookBook/Script/recipes.js
+++ arcade/CookBook/Script/recipes.js
@@ -114,7 +114,7 @@ const RECIPES = [
     categories: { cuisine: 'asian', ingredient: ['veg-protein', 'seafood'], type: ['soups', 'no-carbs'] } // verify seafood addition
   },
   {
-    key: 'SopaDeLentejas',
+    key: 'SopadeLentejas',
     label: 'Sopa de Lentejas Soup',
     image: '../Image/lentilsoup.jpg',
      video: "../vid/bigmacsalad.mp4",
